167d83b07fbdda9f6e23caf0b2f3b42346aae26b
Marta <marciak98@gmail.com>
2023-11-12 21:26:55 +0000

    indending to fix the graph...

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,7 @@ folder_path = os.getcwd()
 file_name = "df_final.csv"
 full_path = os.path.join(folder_path, file_name)
 
-<<<<<<< HEAD
 data = pd.read_csv(full_path)
-=======
-# test importing data from github
-data = pd.read_csv("suicide_data_2019.csv")
->>>>>>> 128123e9d9c5bb39640b8fa5843ac7be5151ca12
 df = pd.DataFrame(data)
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -36,12 +31,14 @@ st.write("Hello, Streamlit!")
 
 # Display head of the DataFrame
 st.write("Healthcare Project DataFrame:")
-<<<<<<< HEAD
 st.write(df)
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 # graph
 # ---------------------------------------------------------------------------------------------------------------------------------------------
-# streamlit method
+# streamlit method - vertical graph
+st.bar_chart(df, y="OBS_VALUE", x="geo", color="top_no")
+
+# streamlit method - horizontal graph
 st.bar_chart(df, x="OBS_VALUE", y="geo", color="top_no")
 
 # matplotlib method
@@ -59,21 +56,21 @@ second_causes = df.loc[df.top_no == '2']
 third_causes = df.loc[df.top_no == '3']
 bar_height = 0.3
 
-plt.barh(y_axis + bar_height, first_causes.OBS_VALUE, height = bar_height)
-plt.barh(y_axis, second_causes.OBS_VALUE, height = bar_height)
-plt.barh(y_axis-bar_height, third_causes.OBS_VALUE, height = bar_height)
+# ax.barh(y_axis + bar_height, first_causes.OBS_VALUE, height = bar_height)
+# ax.barh(y_axis, second_causes.OBS_VALUE, height = bar_height)
+# ax.barh(y_axis-bar_height, third_causes.OBS_VALUE, height = bar_height)
 
-# Add labels to the bars using plt.text
-for i in range(len(countries)):
-    plt.text(first_causes.OBS_VALUE.iloc[i] + 1, i + bar_height, first_causes.icd10.iloc[i], va='center', fontsize=14, style = 'italic')
-    plt.text(second_causes.OBS_VALUE.iloc[i] + 1, i, second_causes.icd10.iloc[i], va='center', fontsize=14, style = 'italic')
-    plt.text(third_causes.OBS_VALUE.iloc[i] + 1, i - bar_height, third_causes.icd10.iloc[i], va='center', fontsize=14, style = 'italic')
+# # Add labels to the bars using plt.text
+# for i in range(len(countries)):
+#     ax.text(first_causes.OBS_VALUE.iloc[i] + 1, i + bar_height, first_causes.icd10.iloc[i], va='center', fontsize=14, style = 'italic')
+#     ax.text(second_causes.OBS_VALUE.iloc[i] + 1, i, second_causes.icd10.iloc[i], va='center', fontsize=14, style = 'italic')
+#     ax.text(third_causes.OBS_VALUE.iloc[i] + 1, i - bar_height, third_causes.icd10.iloc[i], va='center', fontsize=14, style = 'italic')
 
-plt.yticks(y_axis, countries, fontsize=16)
-plt.title("Causes of death per EU country", fontsize=30)
-plt.legend()
+# ax.yticks(y_axis, countries, fontsize=16)
+ax.barh(x, y, 0.5)
+# ax.title("Causes of death per EU country", fontsize=30)
+# ax.legend()
 st.pyplot(fig)
-=======
 
 df_head = df.head()
 st.write(df_head)
@@ -81,4 +78,3 @@ st.write(df_head)
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 # graph
 # ---------------------------------------------------------------------------------------------------------------------------------------------
->>>>>>> 128123e9d9c5bb39640b8fa5843ac7be5151ca12
