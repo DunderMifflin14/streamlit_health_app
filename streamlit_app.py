2ac188d062b28b135c07b80c909bfdc3dee8e5a4
AndrzejReszke <71697919+AndrzejReszke@users.noreply.github.com>
2024-02-11 15:49:08 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import pandas as pd
 import matplotlib.pyplot as plt  # comment out for now as it triggers error
 import os
 import numpy as np
-import seaborn as sns
 
 # custom modules
 from create_graph import CreateGraph, Matlab, Scatter, BarChart
